5725e41c32f938318eaae0c8aa3e3791d4ef4d5c
mdabdulmazidkhan <abdulmazidk0851@gmail.com>
2024-06-05 22:40:13 +0600

    Add files via upload

---
+++ myfirstcalculator.py
@@ -0,0 +1,14 @@
+# Enter the values of a and b to get your result
+a = 1.101010101
+b = 1.24938438
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print("The addition of", a, "and", b, "is", c)
+print("The difference between", a, "and", b, "is", d)
+print("The multiplication between", a, "and", b, "is", e)
+print("The division between", a, "and", b, "is", f)
+# It's my first calculator
+# The model can be more accurate by replacing some codes
+# This model could be more easily usable to users if I add some design, though I don't know how to do that yet
